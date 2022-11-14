efa700aee9bf188e308e452cc9896358efd07204
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-11-14 12:49:49 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -21,9 +21,13 @@ async function fetchData(urlApi) {
     const videos = await fetchData(API);
     let view = `
     ${videos.items.map(video => `
+    <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          
+          
+          
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
